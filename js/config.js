3bdfc6408060578584269f7b162e64b9c91b99df
tai.nguyen <ductai2982@gmail.com>
2025-03-08 01:06:12 +0700

    upload source code

--- js/config.js
+++ js/config.js
@@ -5,4 +5,4 @@ const base64 = "";
 const giftImageBase64 = "data:image/png;base64, " + base64;
 const titleCard = "Linh";
 const contentCard =
-  "Chúc Linh ngày 8/3 tràn ngập niềm vui và những nụ cười. Mong những điều đẹp nhất sẽ đến với Linh trong hôm nay và cả những ngày sau.";
+  "Chúc Linh ngày 8/3 tràn ngập niềm vui và những nụ cười, luôn xinh đẹp, thành công và tràn đầy năng lượng. Mong những điều đẹp nhất sẽ đến với Linh trong hôm nay và cả những ngày sau.";
