236e03dbe19d4f787667c1d8d4a4c1fb377292d3
dunghihua <gamingdung33@gmail.com>
2025-03-08 18:00:12 +0700

    test

--- js/config.js
+++ js/config.js
@@ -3,6 +3,6 @@ const actionName = "ĐẬP HỘP";
 const giftImage = "./image/avatar.jpg";
 const base64 = "";
 const giftImageBase64 = "data:image/png;base64, " + base64;
-const titleCard = "Linh";
+const titleCard = "Quỳnh";
 const contentCard =
-  "Chúc Linh ngày 8/3 tràn ngập niềm vui và những nụ cười, luôn xinh đẹp, thành công và tràn đầy năng lượng. Mong những điều đẹp nhất sẽ đến với Linh trong hôm nay và cả những ngày sau.";
+  "Chúc Quỳnh ngày 8/3 tràn ngập niềm vui và những nụ cười, luôn xinh đẹp, học giỏi và tràn đầy năng lượng. Mong những điều đẹp nhất sẽ đến với quỳnh trong hôm nay và cả những ngày sau.";
